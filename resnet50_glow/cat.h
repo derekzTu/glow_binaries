1e62b3f839fdb64c4638fec3dc7f9d7c619d838d
Derek TU <derek@ours-tech.com>
2019-05-29 20:21:32 -0700

    added cat header

---
+++ resnet50_glow/cat.h
@@ -0,0 +1,7 @@
+Allocated weights of size: 102121984
+Expected weights of size: 102121984
+Loaded weights of size: 102121984 from the file resnet50.weights
+Allocated mutable weight variables of size: 606144
+Loaded images size in bytes is: 602112
+Copying image data into mutable weight vars: 602112 bytes
+Result: 285
